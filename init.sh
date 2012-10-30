dbe38568cf64d806c2e6ac48417088766a1b71f0
Danny Guinther <danny.guinther@leadkarma.com>
2012-10-30 12:44:56 -0400

    Add init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+current_user=`whoami`
+echo "$current_user ALL=NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$current_user 
+sudo chmod 0440 /etc/sudoers.d/$current_user
+#sudo apt-get install -y build-essential git ruby1.9.3 rubygems
+#sudo gem1.9.3 install --no-ri --no-rdoc --version '10.14.2' chef
+sudo mkdir -p /var/log/tdg5_chef
+sudo chown -R $current_user:$current_user /var/log/tdg5_chef
