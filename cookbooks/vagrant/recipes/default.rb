b3ad09054eeecce65907be2c621b4e6ee8d91b72
Danny Guinther <danny.guinther@leadkarma.com>
2012-11-05 12:08:37 -0500

    Prevent vagrant from overwriting vagrant user config

--- cookbooks/vagrant/recipes/default.rb
+++ cookbooks/vagrant/recipes/default.rb
@@ -1,10 +1,5 @@
 include_recipe 'virtualbox'
 
-user node.vagrant.user do
-	action :create
-	shell '/bin/false'
-end
-
 rvm_environment node.vagrant.ruby_string do
 	action :create
 	user node.vagrant.user
