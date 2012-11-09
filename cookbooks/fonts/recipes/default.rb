2c6c1e4c3b1c9a5c86a059528a1c16d1b285e36e
Danny Guinther <danny.guinther@leadkarma.com>
2012-11-09 14:30:39 -0500

    Add design role with gimp and font stuff

---
+++ cookbooks/fonts/recipes/default.rb
@@ -0,0 +1,8 @@
+package 'fontmatrix msttcorefonts'
+
+#remote_directory "/home/#{node.user.username}/.fonts" do
+#	files_group node.user.group
+#	files_owner node.user.username
+#	files_mode '0644'
+#	source 'fonts'
+#end
---
+++ cookbooks/gimp/recipes/default.rb
@@ -0,0 +1,9 @@
+apt_repository 'gimp' do
+	components ['main']
+	distribution node.lsb.codename
+	key '614C4B38'
+	keyserver 'keyserver.ubuntu.com'
+	uri 'http://ppa.launchpad.net/otto-kesselgulasch/gimp/ubuntu'
+end
+
+%w[gimp gimp-plugin-registry gimp-resynthesizer].each { |pkg| package pkg }
