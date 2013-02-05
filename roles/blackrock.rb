0ef39d5b4406dbe40f145e24f21a87433f1025f4
Danny Guinther <dannyguinther@gmail.com>
2013-02-05 07:34:03 -0500

    Update blackrock run list

--- roles/blackrock.rb
+++ roles/blackrock.rb
@@ -3,6 +3,9 @@ description 'Role for blackrock specifics'
 run_list [
 	'role[base]',
 	'recipe[config::filesystem_layout]',
+	'role[design]',
+	'role[dev]',
+	'recipe[audacity]',
 ]
 user = group = 'tdg5'
 default_attributes({
