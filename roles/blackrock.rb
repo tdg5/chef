4f26e6f999f0e56d487a061aac8a15640b3290f4
Danny Guinther <dannyguinther@gmail.com>
2013-02-10 06:15:14 -0500

    Update ruby version in rvmrc and for blackrock

--- roles/blackrock.rb
+++ roles/blackrock.rb
@@ -4,6 +4,7 @@ run_list [
 	'role[base]',
 	'recipe[grub::blackrock]',
 	'recipe[config::filesystem_layout]',
+	'recipe[modprobe]',
 	'role[design]',
 	'role[dev]',
 	'role[rvm]',
@@ -12,6 +13,26 @@ run_list [
 ]
 user = group = 'tdg5'
 default_attributes({
+	:modprobe => {
+		:blacklists => [
+		],
+	},
+	:rvm => {
+		:branch => 'none',
+		:rvmrc => {
+			:rvm_trust_rvmrcs_flag => 1,
+		},
+		:version => '1.18.6',
+		:default_ruby => '1.9.3-p385',
+		:gems => {
+			'1.9.3-p385' => [{
+				:name => 'chef',
+				:version => '11.2.0',
+			}, {
+				:name => 'bundler'
+			}],
+		},
+	},
 	'user' => {
 		'username' => user,
 		'email' => 'dannyguinther@gmail.com',
