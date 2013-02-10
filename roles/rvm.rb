66bfe08c556957634f471e262fcfbf23eb5d1ad0
Danny Guinther <dannyguinther@gmail.com>
2013-02-10 06:19:39 -0500

    Remove rvm config from rvm role

--- roles/rvm.rb
+++ roles/rvm.rb
@@ -3,19 +3,4 @@ description 'Role for nodes with RVM/Ruby'
 run_list [
 	'recipe[rvm::system]',
 ]
-default_attributes({
-	:rvm => {
-		:branch => 'none',
-		:rvmrc => {
-			:rvm_trust_rvmrcs_flag => 1,
-		},
-		:version => '1.17.2',
-		:default_ruby => '1.9.3-p327',
-		:gems => {
-			'1.9.3-p327' => [{
-				:name => 'chef',
-				:version => '10.16.4',
-			},],
-		},
-	},
-})
+default_attributes({})
