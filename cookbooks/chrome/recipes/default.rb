6bc4253e4279c7f8581f6f4e4e61256a220b2709
Danny Guinther <dannyguinther@gmail.com>
2013-02-10 06:25:50 -0500

    Install chrome from deb instead of repository

--- cookbooks/chrome/recipes/default.rb
+++ cookbooks/chrome/recipes/default.rb
@@ -1,7 +1,21 @@
-apt_repository 'google-chrome' do
-	components ['stable', 'main']
-	key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
-	uri 'http://dl.google.com/linux/chrome/deb/'
+# Chome will add its own repo after installation so method below is preferred,
+# but untested. Delete this after testing dpkg install.
+#apt_repository 'google-chrome' do
+#	components ['stable', 'main']
+#	key 'https://dl-ssl.google.com/linux/linux_signing_key.pub'
+#	uri 'http://dl.google.com/linux/chrome/deb/'
+#end
+#
+#package 'google-chrome-stable'
+
+remote_file '/tmp/google-chrome-stable_current_amd64.deb' do
+	backup false
+	notifies :install, 'dpkg_package[google-chrome]'
+	source 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb'
+	not_if 'which google-chrome'
 end
 
-package 'google-chrome-stable'
+dpkg_package 'google-chrome' do
+	action :nothing
+	source '/tmp/google-chrome-stable_current_amd64.deb'
+end
