9b69e11854237acf83e8f84c409452175a8c5e02
Danny Guinther <dannyguinther@gmail.com>
2013-04-20 17:39:21 -0400

    Remove vim plugins you_complete_me and powerline

--- cookbooks/vim_plugins/recipes/powerline.rb
+++
@@ -1,6 +0,0 @@
-git "/home/#{node.user.username}/#{node.vim_plugins.user_bundle_dir}/powerline" do
-	group node.user.group
-	repository 'https://github.com/Lokaltog/vim-powerline.git'
-	revision '09c0cea859a2e0989eea740655b35976d951a84e'
-	user node.user.username
-end
