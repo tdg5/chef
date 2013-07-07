c25e376998d538c98222c2d850285f5bf9848683
Danny Guinther <dannyguinther@gmail.com>
2013-07-07 06:23:29 -0400

    Remove dropbox ppa

--- cookbooks/dropbox/recipes/default.rb
+++ cookbooks/dropbox/recipes/default.rb
@@ -1,8 +1,1 @@
-apt_repository 'dropbox' do
-	components ['main']
-	distribution node.lsb.codename
-	key '5044912E'
-	keyserver 'pgp.mit.edu'
-	uri 'http://linux.dropbox.com/ubuntu'
-end
 package 'nautilus-dropbox'
