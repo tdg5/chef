1a28c8df6defdef4261939ee8c4da5c056d592a2
Danny Guinther <dannyguinther@gmail.com>
2013-07-20 10:02:03 -0400

    Add Firefox to favorite apps

--- cookbooks/gnome/recipes/gsettings.rb
+++ cookbooks/gnome/recipes/gsettings.rb
@@ -1,4 +1,4 @@
-gsettings_code = %q{gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'pidgin.desktop', 'spotify.desktop', 'rhythmbox.desktop', 'nautilus.desktop', 'gnome-terminal.desktop', 'gimp.desktop', 'virtualbox.desktop']"}
+gsettings_code = %q{gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'pidgin.desktop', 'spotify.desktop', 'rhythmbox.desktop', 'nautilus.desktop', 'gnome-terminal.desktop', 'gimp.desktop', 'virtualbox.desktop', 'firefox.desktop']"}
 
 bash 'set_gsettings_values' do
 	code gsettings_code
