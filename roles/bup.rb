ff2cf5e558b7327d48890f91622f1c3449798be3
Danny Guinther <dannyguinther@gmail.com>
2013-07-22 11:01:08 -0400

    Add bup role and node json

---
+++ roles/bup.rb
@@ -0,0 +1,33 @@
+name 'bup'
+description 'Role for bup specifics'
+run_list [
+	'role[base]',
+	'recipe[rvm]',
+	#'role[vm_host]',
+	'recipe[qt::dev]',
+	'role[design]',
+	'role[dev]',
+]
+user = group = 'danny'
+default_attributes({
+	'rvm' => {
+		'branch' => 'none',
+		:default_ruby => '',
+		:rubies => [
+			{
+				:version => '1.9.3-p385',
+				:patch => 'falcon',
+			},
+		],
+		'rvmrc' => {
+			'rvm_trust_rvmrcs_flag' => 1,
+		},
+		'version' => '1.18.6',
+	},
+	'user' => {
+		'username' => user,
+		'email' => 'danny@backupify.com',
+		'group' => group,
+	}
+})
+
