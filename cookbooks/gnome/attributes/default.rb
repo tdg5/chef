3fa108746f4730600ae0950d79cd65e315268c08
Danny Guinther <danny@backupify.com>
2013-07-25 11:44:48 -0400

    Improve gnome:gsettings and add home row workspace switching bindings

---
+++ cookbooks/gnome/attributes/default.rb
@@ -0,0 +1,23 @@
+default.gnome = {
+  :gsettings => [{
+    :key => 'favorite-apps',
+    :schema => 'org.gnome.shell',
+    :value => "['google-chrome.desktop', 'pidgin.desktop', 'spotify.desktop', 'rhythmbox.desktop', 'nautilus.desktop', 'gnome-terminal.desktop', 'gimp.desktop', 'virtualbox.desktop', 'firefox.desktop']",
+  }, {
+    :key => 'move-to-workspace-down',
+    :schema => 'org.gnome.desktop.wm.keybindings',
+    :value => "['<Control><Shift><Alt>Down', '<Control><Shift><Alt>j']",
+  }, {
+    :key => 'move-to-workspace-up',
+    :schema => 'org.gnome.desktop.wm.keybindings',
+    :value => "['<Control><Shift><Alt>Down', '<Control><Shift><Alt>k']",
+  }, {
+    :key => 'switch-to-workspace-up',
+    :schema => 'org.gnome.desktop.wm.keybindings',
+    :value => "['<Control><Alt>Up', '<Control><Alt>k']",
+  }, {
+    :key => 'switch-to-workspace-down',
+    :schema => 'org.gnome.desktop.wm.keybindings',
+    :value => "['<Control><Alt>Down', '<Control><Alt>j']",
+  },],
+}
--- cookbooks/gnome/recipes/gsettings.rb
+++ cookbooks/gnome/recipes/gsettings.rb
@@ -1,6 +1,13 @@
-gsettings_code = %q{gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'pidgin.desktop', 'spotify.desktop', 'rhythmbox.desktop', 'nautilus.desktop', 'gnome-terminal.desktop', 'gimp.desktop', 'virtualbox.desktop', 'firefox.desktop']"}
+gsgsettings_code = ''
+node.gnome.gsettings.each do |gsetting|
+  current_setting_value = `sudo -u #{node.user.username} -- gsettings get #{gsetting[:schema]} #{gsetting[:key]} 2>/dev/null`.strip
+  next if current_setting_value == gsetting[:value]
+  gsettings_code += %Q[gsettings set #{gsetting[:schema]} #{gsetting[:key]} "#{gsetting[:value]}"\n]
+end
 
-bash 'set_gsettings_values' do
-	code gsettings_code
-	user node.user.username
+unless gsettings_code.empty?
+  bash 'set_gsettings_values' do
+    code gsettings_code
+    user node.user.username
+  end
 end
