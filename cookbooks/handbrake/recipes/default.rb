a83166063f5e55951c4255773733d407f440628f
Danny Guinther <dannyguinther@gmail.com>
2013-08-26 08:09:36 -0400

    Fix handbrake keyserver

--- cookbooks/handbrake/recipes/default.rb
+++ cookbooks/handbrake/recipes/default.rb
@@ -2,7 +2,7 @@ apt_repository 'handbrake' do
 	components ['main']
 	distribution node.lsb.codename
 	key '816950D8'
-	keyserver 'server keyserver.ubuntu.com'
+	keyserver 'keyserver.ubuntu.com'
 	not_if 'apt-cache policy | grep -q stebbins/handbrake-releases'
 	uri 'http://ppa.launchpad.net/stebbins/handbrake-releases/ubuntu'
 end
