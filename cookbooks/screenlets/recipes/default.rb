9d2c0b25ac25aa72573ecf614df3a6c5ccfc935f
Danny Guinther <dannyguinther@gmail.com>
2013-09-08 13:40:09 -0400

    Add key for screenlets ppa

--- cookbooks/screenlets/recipes/default.rb
+++ cookbooks/screenlets/recipes/default.rb
@@ -2,6 +2,7 @@ apt_repository 'screenlets-ppa-precise' do
 	components ['main']
 	distribution node.lsb.codename
 	keyserver 'keyserver.ubuntu.com'
+  key 'A8670E8C'
 	not_if 'apt-cache policy | grep -q screenlets/ppa/ubuntu'
 	uri 'http://ppa.launchpad.net/screenlets/ppa/ubuntu'
 end
