95760838dc852499b4e1c03ee16b4e678c54f1f2
Danny Guinther <dannyguinther@gmail.com>
2013-12-22 07:37:37 -0500

    Only remove overlay-scrollbars if installed

--- cookbooks/gnome/recipes/disable_overlay_scrollbars.rb
+++ cookbooks/gnome/recipes/disable_overlay_scrollbars.rb
@@ -1,5 +1,6 @@
 %w[overlay-scrollbar liboverlay-scrollbar-0.2-0 liboverlay-scrollbar3-0.2-0].each do |pkg|
-	package pkg do
-		action :remove
-	end
+ package pkg do
+  action :remove
+  only_if "dpkg -s #{pkg} >/dev/null 2>&1"
+ end
 end
