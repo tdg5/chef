9b6dec68077f3f5f87f417e151f5299ccb760d60
Danny Guinther <dannyguinther@gmail.com>
2013-12-22 10:35:01 -0500

    Add node and role for surface

---
+++ roles/surface.rb
@@ -0,0 +1,45 @@
+name 'surface'
+description 'Role for surface specifics'
+run_list [
+  'role[base]',
+  'role[tools]',
+  'role[design]',
+  'role[dev]',
+  'role[rvm]',
+  'recipe[java]',
+  'role[nodejs]',
+  'recipe[lastpass]',
+]
+user = group = 'tdg5'
+default_attributes({
+  :modprobe => {
+    :blacklists => [
+    ],
+  },
+  :rvm => {
+    :branch => 'none',
+    :rvmrc => {
+      :rvm_trust_rvmrcs_flag => 1,
+    },
+    :version => '1.25.5',
+    :default_ruby => '1.9.3-p484',
+    :gems => {},
+    :global_gems => [{
+      :name => 'gem-ctags',
+    }, {
+      :name => 'chef',
+      :version => '11.8.2',
+    }, {
+      :name => 'bundler'
+    },],
+    'rubies' => [
+      '1.9.3-p484',
+      '2.0.0-p353',
+    ],
+  },
+  'user' => {
+    'username' => user,
+    'email' => 'dannyguinther@gmail.com',
+    'group' => group,
+  }
+})
