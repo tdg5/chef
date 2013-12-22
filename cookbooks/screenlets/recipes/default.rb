13db0af47ca21c7ccca40ce459cd19280976baa0
Danny Guinther <dannyguinther@gmail.com>
2013-12-22 07:52:42 -0500

    Only add screenlets ppa on older ubuntus

--- cookbooks/screenlets/recipes/default.rb
+++ cookbooks/screenlets/recipes/default.rb
@@ -1,10 +1,12 @@
-apt_repository 'screenlets-ppa-precise' do
-	components ['main']
-	distribution node.lsb.codename
-	keyserver 'keyserver.ubuntu.com'
-  key 'A8670E8C'
-	not_if 'apt-cache policy | grep -q screenlets/ppa/ubuntu'
-	uri 'http://ppa.launchpad.net/screenlets/ppa/ubuntu'
+if node['platform'] == 'ubuntu' && Chef::VersionConstraint.new('<= 12.04').include?(node['platform_version'])
+  apt_repository 'screenlets-ppa-precise' do
+    components ['main']
+    distribution node.lsb.codename
+    keyserver 'keyserver.ubuntu.com'
+    key 'A8670E8C'
+    not_if 'apt-cache policy | grep -q screenlets/ppa/ubuntu'
+    uri 'http://ppa.launchpad.net/screenlets/ppa/ubuntu'
+  end
 end
 
 package 'screenlets'
