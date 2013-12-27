1b552ef35aa7572560229161602e84f299855f7e
Danny Guinther <dannyguinther@gmail.com>
2013-12-27 06:26:51 -0500

    Install gnome version specific extensions

---
+++ cookbooks/gnome/recipes/extensions_3.2.rb
@@ -0,0 +1,27 @@
+extensions = %w[
+  RecentItems@bananenfisch.net
+  alternative-status-menu@gnome-shell-extensions.gcampax.github.com
+  antisocial-menu@cnervi.github.com
+  avoidoverview@rastersoft.com
+  impatience@gfxmonk.net
+  mediaplayer@patapon.info
+  nohotcorner@azuri.free.fr
+  places-menu@gnome-shell-extensions.gcampax.github.com
+  removeaccesibility@lomegor
+  status-area-horizontal-spacing@mathematical.coffee.gmail.com
+  system-monitor@paradoxxx.zero.gmail.com
+]
+
+%w[gir1.2-gtop-2.0 gir1.2-networkmanager-1.0].each {|pkg| package pkg}
+
+extensions.each do |extension|
+  extension_dir = "/home/#{node.user.username}/.local/share/gnome-shell/extensions/#{extension}"
+
+  remote_directory extension_dir do
+    files_group node.user.group
+    files_owner node.user.username
+    files_mode '0644'
+    not_if "file -d #{extension_dir}"
+    source "extensions/3.2/#{extension}"
+  end
+end
