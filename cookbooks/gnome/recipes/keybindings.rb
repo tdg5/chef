0bc9534181bef4658e9db87357dbc477cfd4d07d
Danny Guinther <dannyguinther@gmail.com>
2013-12-27 05:49:44 -0500

    Use dconf to create keybindings on newer gnomes

--- cookbooks/gnome/recipes/keybindings.rb
+++ cookbooks/gnome/recipes/keybindings.rb
@@ -1,17 +1,16 @@
 code = []
 user_gconf_config_source = "xml::/home/#{node.user.username}/.gconf"
 
-gnome_version = `gnome-session --version`.strip.split(/ /).last
-
 node.gnome.keybindings.each_with_index do |keybinding, index|
-  if gnome_version < '3.4'
+  if GnomeVersion.gnome_version_constraint('< 3.4')
     existing_value = `gconftool --config-source #{user_gconf_config_source} --get /desktop/gnome/keybindings/custom#{index}/name`.strip
     next if existing_value == keybinding[0]
     code << "gconftool --config-source #{user_gconf_config_source} --set /desktop/gnome/keybindings/custom#{index}/name --type string '#{keybinding[0]}'"
     code << "gconftool --config-source #{user_gconf_config_source} --set /desktop/gnome/keybindings/custom#{index}/action --type string '#{keybinding[1]['action']}'"
     code << "gconftool --config-source #{user_gconf_config_source} --set /desktop/gnome/keybindings/custom#{index}/binding --type string '#{keybinding[1]['binding']}'"
   else
-    existing_value = `dconf read /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom#{index}/name`.strip
+    # dconf returns quoted expression
+    existing_value = `dconf read /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom#{index}/name`.strip.gsub(/(^'|'$)/, '')
     next if existing_value == keybinding[0]
     code << "dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom#{index}/name \"'#{keybinding[0]}'\""
     code << "dconf write /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom#{index}/command \"'#{keybinding[1]['action']}'\""
@@ -19,10 +18,9 @@ node.gnome.keybindings.each_with_index do |keybinding, index|
   end
 end
 
-unless code.empty?
-  gconf_code = code.join(' && ')
-  bash 'install_keybindings' do
-    code gconf_code
-    user node.user.username
-  end
+gconf_code = code.join(' && ')
+bash 'install_keybindings' do
+  code gconf_code
+  user node.user.username
+  not_if { code.empty? }
 end
