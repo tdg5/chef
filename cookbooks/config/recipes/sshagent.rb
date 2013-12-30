f22d8998f317b2ed4986d4973dc4e0d883d3fc5f
Danny Guinther <dannyguinther@gmail.com>
2013-12-30 07:38:57 -0500

    Update .sshagentrc and correctly delete X11 ssh-agent script

--- cookbooks/config/recipes/sshagent.rb
+++ cookbooks/config/recipes/sshagent.rb
@@ -1,10 +1,24 @@
 cookbook_file "/home/#{node.user.username}/.sshagentrc" do
-	group node.user.group
-	owner node.user.username
-	mode 0744
+  group node.user.group
+  owner node.user.username
+  mode 0744
 end
 
-file '/etc/X11/Xsession.d/90x11-common_ssh-agent' do
-	action :delete
-	only_if 'file -e /etc/X11/Xsession.d/90x11-common_ssh-agent'
+# Prevent various other services from starting ssh-agent
+{
+  'X11' => '/etc/X11/Xsession.d/90x11-common_ssh-agent',
+  'gnome_keyring' => '/etc/xdg/autostart/gnome-keyring-ssh.desktop',
+}.each_pair do |init_owner, init_file|
+  bash "remove_#{init_owner}_ssh_agent_initializer" do
+    code "mv #{init_file} #{init_file}.bak"
+    only_if "test -e #{init_file}"
+    user 'root'
+  end
+end
+
+xsession_options_path = '/etc/X11/Xsession.options'
+bash 'prevent_Xsession_from_starting_ssh_agent' do
+  code "sed -i 's/^use-ssh-agent/# use-ssh-agent/' #{xsession_options_path}"
+  only_if "grep '^use-ssh-agent' #{xsession_options_path}"
+  user 'root'
 end
