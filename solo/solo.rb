83c6d65add2e0ebe9c1c2e499c8f9c2a44897ff6
Danny Guinther <dannyguinther@gmail.com>
2014-02-06 09:47:27 -0500

    Update chef solo to use all local paths

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,8 +1,12 @@
-dir = File.expand_path(File.dirname(__FILE__))
-cookbook_path "#{dir}/../cookbooks"
+root_dir = File.expand_path('../..', __FILE__)
+home_dir = `echo $HOME`.strip
+
+cookbook_path "#{root_dir}/cookbooks"
+data_bag_path "#{root_dir}/data_bags"
+encrypted_data_bag_secret "#{home_dir}/.ssh/id_rsa.home"
 file_cache_path '/tmp/chef/solo'
-log_location '/var/log/tdg5_chef/chef-solo.log'
-node_path "#{dir}/nodes"
-role_path "#{dir}/../roles"
+log_location "#{root_dir}/log/chef-solo.log"
+node_path "#{root_dir}/solo/nodes"
+role_path "#{root_dir}/roles"
 log_level :debug
 verbose_logging true
