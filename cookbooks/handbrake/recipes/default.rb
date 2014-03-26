99bfbde1cf0f0198039011363281a9c57bf73bb8
Danny Guinther <dannyguinther@gmail.com>
2014-03-26 17:35:14 -0400

    Update handbrake cookbook for trusty

--- cookbooks/handbrake/recipes/default.rb
+++ cookbooks/handbrake/recipes/default.rb
@@ -1,10 +1,1 @@
-apt_repository 'handbrake' do
-  components ['main']
-  distribution node.lsb.codename
-  key '816950D8'
-  keyserver 'keyserver.ubuntu.com'
-  not_if 'apt-cache policy | grep -q stebbins/handbrake-releases'
-  uri 'http://ppa.launchpad.net/stebbins/handbrake-releases/ubuntu'
-end
-
-package 'handbrake-gtk'
+package 'handbrake'
