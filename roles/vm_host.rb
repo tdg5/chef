f9a11300f6970295bf4988ea45f2b2bb4af5344b
Danny Guinther <danny@backupify.com>
2014-05-23 13:25:20 -0400

    Add vagrant cookbook and add to vm_host role

--- roles/vm_host.rb
+++ roles/vm_host.rb
@@ -2,4 +2,11 @@ name 'vm_host'
 description 'Role for VirtualBox based vm host'
 run_list [
   'recipe[virtualbox]',
+  'recipe[vagrant]',
 ]
+default_attributes({
+  'vagrant' => {
+    'url' => 'http://files.vagrantup.com/packages/a40522f5fabccb9ddabad03d836e120ff5d14093/vagrant_1.3.5_x86_64.deb',
+    'checksum' => 'db7d06f46e801523d97b6e344ea0e4fe942f630cc20ab1706e4c996872f8cd71',
+  },
+})
