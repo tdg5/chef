54233e38671186552c4c50abbe17bb190b6abcff
Danny Guinther <dannyguinther@gmail.com>
2014-09-12 08:54:22 -0400

    Add cookbook for packer and add to vm_host role

--- roles/vm_host.rb
+++ roles/vm_host.rb
@@ -3,10 +3,14 @@ description 'Role for VirtualBox based vm host'
 run_list [
   'recipe[virtualbox]',
   'recipe[vagrant]',
+  'recipe[packer]',
 ]
 default_attributes({
   'vagrant' => {
     'url' => 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5_x86_64.deb',
     'checksum' => 'e2c7af6d032ac551ebd6399265cb9cb64402c9fb96a12289161b7f67afada28a',
+  },
+  'packer' => {
+    'version' => '0.7.1',
   },
 })
