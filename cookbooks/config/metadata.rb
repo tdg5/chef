904d4ae2489d19570f37888e4887794c79bb116d
Danny Guinther <dannyguinther@gmail.com>
2014-10-24 06:42:09 -0400

    Add metadata and default recipe to config cookbook

---
+++ cookbooks/config/metadata.rb
@@ -0,0 +1,17 @@
+name 'config'
+maintainer 'Danny Guinther'
+maintainer_email  'dannyguinther@gmail.com'
+license           'MIT'
+description       'Various configuration files and tweaks'
+long_description  'Various configuration files and tweaks'
+version           '0.0.1'
+
+recipe 'config', 'noop'
+recipe 'config:git', 'Installs user git config'
+recipe 'config:gnome-terminal', 'Installs gnome terminal config'
+recipe 'config:irb', 'Installs configuration for Ruby irb'
+recipe 'config:sshagent', 'Installs sshagent automatic ssh key loader script'
+recipe 'config:vim', 'Installs vim config'
+
+supports 'debian'
+supports 'ubuntu'
