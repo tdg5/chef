83f8fe20381b19e659ecb4e1b6f71a7868841074
Danny Guinther <dannyguinther@gmail.com>
2014-11-15 09:50:27 -0500

    Add initial config for the_machine

---
+++ roles/the_machine.rb
@@ -0,0 +1,34 @@
+name 'blackrock'
+description 'Role for blackrock specifics'
+run_list [
+  'role[rvm]',
+  'role[vm_host]',
+  'role[docker]',
+]
+user = group = 'patrick'
+
+default_attributes({
+  :openssh => {
+    :server => {
+      :password_authentication => 'no',
+    },
+  },
+  :rvm => {
+    :branch => 'none',
+    :rvmrc => {
+      :rvm_trust_rvmrcs_flag => 1,
+      :rvmsudo_secure_path => 1,
+    },
+    :version => '1.26.1',
+    :default_ruby => '2.1.4',
+    :gems => {
+      '2.1.4' => default_gems,
+    },
+    :global_gems => [{
+      :name => 'gem-ctags',
+    }],
+    :rubies => [
+      '2.1.4',
+    ],
+  },
+})
