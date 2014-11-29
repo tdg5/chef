f73430334d2105ed6b91c98c99dd03881d853037
Danny Guinther <dannyguinther@gmail.com>
2014-11-29 11:39:48 -0500

    Add knife-solo and restructure to give it control

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,16 +1,18 @@
 root_dir = File.expand_path('../..', __FILE__)
-home_dir = `echo $HOME`.strip
 
 cookbook_path(%W[
   #{root_dir}/cookbooks
-  #{root_dir}/vendor/cookbooks
+  #{root_dir}/local_cookbooks
 ])
 
 data_bag_path "#{root_dir}/data_bags"
-encrypted_data_bag_secret "#{home_dir}/.ssh/id_rsa.home"
+encrypted_data_bag_secret File.join(ENV['HOME'] || '~', '.ssh', 'id_rsa')
 file_cache_path "#{root_dir}/.cache"
 log_location "#{root_dir}/log/chef-solo.log"
-node_path "#{root_dir}/solo/nodes"
+node_path "#{root_dir}/nodes"
 role_path "#{root_dir}/roles"
 log_level :debug
 verbose_logging true
+
+knife[:berkshelf_path] = 'cookbooks'
+knife[:provisioning_path] = '~/.chef-solo'
--- Gemfile
+++ Gemfile
@@ -1,9 +1,11 @@
 source 'https://rubygems.org'
 
+gem 'berkshelf'
+gem 'knife-solo'
+gem 'knife-solo_data_bag'
+
 group :development do
   gem 'kitchen-vagrant'
   gem 'pry'
   gem 'test-kitchen'
 end
-
-gem 'berkshelf'
