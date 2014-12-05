608026ac30f1fed186f5a2f2b2f504065af0019d
Danny Guinther <dannyguinther@gmail.com>
2014-12-05 08:00:23 -0500

    Add vim_testkey vim plugin

---
+++ local_cookbooks/vim_plugins/recipes/vim_testkey.rb
@@ -0,0 +1,6 @@
+git "/home/#{node.user.username}/#{node.vim_plugins.user_bundle_dir}/vim-testkey" do
+  group node.user.group
+  repository 'https://github.com/botandrose/vim-testkey.git'
+  revision 'dcbd3dd81d9bafbfed960df166db48ce7fdfb324'
+  user node.user.username
+end
