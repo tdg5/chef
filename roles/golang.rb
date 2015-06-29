ddf78f99b752b46b9221578c590ca85557f0732e
Danny Guinther <dannyguinther@gmail.com>
2015-06-29 07:21:24 -0400

    Add golang role

---
+++ roles/golang.rb
@@ -0,0 +1,11 @@
+name "golang"
+description "Role for golang programming language"
+run_list([
+  "recipe[golang]",
+])
+default_attributes({
+  :go => {
+    :platform => "amd64",
+    :version => "1.4",
+  },
+})
