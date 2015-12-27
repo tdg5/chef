ba3bb583048ed6f7be3a7175ac04b94036e5f440
Danny Guinther <dannyguinther@gmail.com>
2015-12-27 07:41:00 -0500

    Replace docker with docker_service

--- roles/docker.rb
+++ roles/docker.rb
@@ -1,14 +1,8 @@
 name 'docker'
 description 'Role for hosts that utilize docker'
 run_list [
-  'recipe[docker]',
+  'recipe[docker_service]',
   'recipe[device-mapper]',
 ]
 default_attributes({
-  'docker' => {
-    'package' => {
-      'repo_url' => 'https://get.docker.io/ubuntu',
-    },
-    'version' => '1.3.0',
-  },
 })
