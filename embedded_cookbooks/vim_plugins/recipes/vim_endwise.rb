886487bca7581ca19ec4ea683cf045059bea922c
Danny Guinther <dannyguinther@gmail.com>
2016-01-24 14:01:47 -0500

    Add vim-endwise plugin recipe

---
+++ embedded_cookbooks/vim_plugins/recipes/vim_endwise.rb
@@ -0,0 +1,6 @@
+git "/home/#{node.user.username}/#{node.vim_plugins.user_bundle_dir}/vim_endwise" do
+  group node.user.group
+  repository "https://github.com/tpope/vim-endwise.git"
+  revision "0067ceda37725d01b7bd5bf249d63b1b5d4e2ab4"
+  user node.user.username
+end
