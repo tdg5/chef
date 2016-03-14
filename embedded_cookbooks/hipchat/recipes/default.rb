519051c2c1bff601daf7c99931f884aaea070e38
Danny Guinther <danny@backupify.com>
2016-03-14 09:20:21 -0400

    Update hipchat recipe with new apt repo

--- embedded_cookbooks/hipchat/recipes/default.rb
+++ embedded_cookbooks/hipchat/recipes/default.rb
@@ -1,8 +1,8 @@
 apt_repository 'atlassian-hipchat' do
-  components %w[stable main]
-  key 'https://www.hipchat.com/keys/hipchat-linux.key'
-  not_if 'apt-cache policy | grep -q downloads.hipchat.com'
-  uri 'http://downloads.hipchat.com/linux/apt'
+  components %w[trusty main]
+  key 'https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public'
+  not_if 'apt-cache policy | grep -q hipchat-apt-client'
+  uri 'https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client'
 end
 
 package 'hipchat'
