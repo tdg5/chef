79501e091a881aa81f22c1553bbf6bd548bf3694
Danny Guinther <dannyguinther@gmail.com>
2016-04-23 16:34:19 -0400

    Make HipChat more release agnostic

--- embedded_cookbooks/hipchat/recipes/default.rb
+++ embedded_cookbooks/hipchat/recipes/default.rb
@@ -1,5 +1,7 @@
+release = node['lsb']['codename']
+
 apt_repository 'atlassian-hipchat' do
-  components %w[trusty main]
+  components [release, "main"]
   key 'https://atlassian.artifactoryonline.com/atlassian/api/gpg/key/public'
   not_if 'apt-cache policy | grep -q hipchat-apt-client'
   uri 'https://atlassian.artifactoryonline.com/atlassian/hipchat-apt-client'
