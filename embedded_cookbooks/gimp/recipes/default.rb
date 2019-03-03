bc018b52e4d89dfae942b280c0b00a492f531970
Danny Guinther <dannyguinther@gmail.com>
2019-03-03 15:22:37 -0500

    Install gimp from custom PPA

--- embedded_cookbooks/gimp/recipes/default.rb
+++ embedded_cookbooks/gimp/recipes/default.rb
@@ -1,1 +1,10 @@
-%w[gimp gimp-plugin-registry gimp-gmic].each { |pkg| package pkg }
+apt_repository 'gimp' do
+  components ['main']
+  distribution node['lsb']['codename']
+  key '614C4B38'
+  keyserver 'keyserver.ubuntu.com'
+  not_if 'apt-cache policy | grep -q otto-kesselgulasch/gimp'
+  uri 'http://ppa.launchpad.net/otto-kesselgulasch/gimp/ubuntu'
+end
+
+%w[gimp gimp-gmic].each { |pkg| package pkg }
